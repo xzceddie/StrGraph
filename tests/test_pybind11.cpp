5dcb3d7c572ffa35ab0376aae02a235692eba2ef
xzceddie <chengxzhcuhk@gmail.com>
2024-04-21 13:32:52 +0800

    Add functional interfaces in pybind11 test

--- tests/test_pybind11.cpp
+++ tests/test_pybind11.cpp
@@ -1,4 +1,10 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/complex.h>
+#include <pybind11/functional.h>
+#include <functional>
+#include <iostream>
+
 namespace py = pybind11;
 
 
@@ -9,8 +15,33 @@ int add(int i, int j) {
 int subtract(int i, int j) {
     return i - j;
 }
+
+using CallbackFunction = std::function<int(int)>;
+
+class MyInterface {
+    CallbackFunction mCallBack;
+public:
+    void setCallback(const CallbackFunction& callback);
+    int performOperation(int value);
+};
+
+void MyInterface::setCallback(const CallbackFunction& callback) {
+    mCallBack = callback;
+}
+
+int MyInterface::performOperation(int value) {
+    std::cout << "(Cpp) Hello, I accept input: " << value << std::endl;
+    return mCallBack(value);
+}
+
 PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
     m.def("add", &add, "A function that adds two numbers");
     m.def("subtract", &subtract, "A function that adds two numbers");
+
+    py::class_<MyInterface>(m, "MyInterface")
+        .def(py::init<>())
+        .def("setCallback", &MyInterface::setCallback)
+        .def("performOperation", &MyInterface::performOperation);
 }
+
--- tests/test_pybind11.py
+++ tests/test_pybind11.py
@@ -1,6 +1,11 @@
 import unittest
 import example  # Import your Pybind11 module here
 
+# For functional test
+def callback_function(value):
+    print(f"(Python) Callback function called with value: {value}")
+    return value * 2;
+
 class MyModuleTestCase(unittest.TestCase):
     def test_addition(self):
         result = example.add(2, 3)  # Call functions from your Pybind11 module
@@ -9,6 +14,11 @@ class MyModuleTestCase(unittest.TestCase):
     def test_subtraction(self):
         result = example.subtract(5, 2)
         self.assertEqual(result, 3)
+
+    def test_callback( self ):
+        tmp_interface = example.MyInterface()
+        tmp_interface.setCallback( callback_function )
+        self.assertEqual( tmp_interface.performOperation( 10 ), 20 )
 
 if __name__ == '__main__':
     unittest.main()
